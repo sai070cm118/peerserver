986160d0ed9c5719d95ff04f220ea49c0266430e
Sairam Vinnakota <sai070cm118@gmail.com>
2020-05-03 20:21:47 +0530

    sdfsdf

--- public/source/js/script.js
+++ public/source/js/script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		 * The iceServers on this example are public and can be used for your project.
 		 */
 		peer = new Peer(peerId,{
-			host: "localhost",
+			host: "https://peernodetest.herokuapp.com/",
 			port: portNo,
 			path: '/mypeer'
 		});
